ed5e11f160a6d6e70a070d02b258f1d4484ec463
Christo <jpcroucamp@gmail.com>
2021-10-22 12:14:00 +0200

    Application Creation

---
+++ src/test/java/com/croucampcc/webphotos/WebPhotosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.croucampcc.webphotos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPhotosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
